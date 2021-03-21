65010fab3cbf9be36a9ec3887edef10ee71325ae
Chitsanupong <first30952@gmail.com>
2021-03-21 17:47:00 +0700

    Finish all design, responsive and functionality

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,41 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Values from 'values.js'
 
 import SingleColor from './components/SingleColor'
+import Footer from './components/Footer'
 import './sass/main.scss'
 
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#61DAFB').all(10));
+
+  const inputElement = useRef(null);
+  useEffect(() => {
+    if (inputElement.current) {
+      inputElement.current.focus();
+    }
+  }, []);
   
   const handleSubmit = (event) => {
     event.preventDefault();
     try {
       let colors = new Values(color).all(10);
       setList(colors)
+      setError(false)
     } catch (error) {
       setError(true)
-      console.log(error);
+
     }
-    
   }
 
   return (
-    <div className="container">
-        <form onSubmit={handleSubmit} className="Form">
-            <input type="text" 
-                   value={color}
-                   placeholder="#f15025" 
-                   onChange={(e) => setColor(e.target.value)}/>
-            <button type="submit">submit</button>
-        </form>
+    <>
+        <div className="container">
+          <h2 className="title">Color Generator</h2>
+          <form onSubmit={handleSubmit} className="Form">
+              <input type="text" 
+                    ref={inputElement}
+                    className="Form__input"
+                    value={color}
+                    placeholder="#61DAFB" 
+                    onChange={(e) => setColor(e.target.value)}/>
+              <button type="submit" className="Form__button">submit</button>
+          </form>
+        </div>
         <section className="colors">
           {list.map((color, index) => {
             return <SingleColor key={index} {...color} index={index}/>
           })}
         </section>
-    </div>
+        <Footer/>
+    </>
   );
 }
 
